4a6e452a35d1a7995af271b9914fc80dbafadd18
Sergey <sergey-sachko97@mail.ru>
2022-07-06 20:17:41 +0300

    added main.go

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"net/http"
+)
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hi. Thats enough for first commit (:")
+	//fmt.Println("Hi. Thats enough for first commit (:")
+
+	url := "https://megasport.msk.ru/afisha-meropriyatiy/"
+
+	response, error := http.Get(url)
+	defer response.Body.Close()
+	checkError(error)
+
+	if response.StatusCode > 400 {
+		fmt.Println("Status code:", response.StatusCode)
+	}
+
+	doc, error := goquery.NewDocumentFromReader(response.Body)
+	checkError(error)
+
+	elementor := doc.Find("div.elementor").Size()
+
+	fmt.Println(elementor)
 }
